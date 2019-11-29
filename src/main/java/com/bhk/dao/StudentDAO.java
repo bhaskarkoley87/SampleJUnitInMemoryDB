6ac9de80662c6f68f0f60f102c7e03a15c7ca9f0
Bhaskar Koley <42509000+bhaskarkoley87@users.noreply.github.com>
2019-11-29 16:27:48 +0530

    Changes for Test

--- src/main/java/com/bhk/dao/StudentDAO.java
+++ src/main/java/com/bhk/dao/StudentDAO.java
@@ -1,39 +1,42 @@
 package com.bhk.dao;
 
 import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-
 import com.bhk.bean.Student;
 import com.bhk.config.HibernateUtil;
 
 public class StudentDAO {
-	private Student student;
-	private Transaction transaction = null;
-	
-	public boolean addStudent(Student student) {
-		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            // start a transaction
-            transaction = session.beginTransaction();
-            // save the student object
-            session.save(student);
-            // commit transaction
-            transaction.commit();
-            return true;
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-            return false;
-        }
-	}
-	
-	public List<Student> getStudents() {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            return session.createQuery("from Student", Student.class).list();
-        }
+  private Student student;
+  private Transaction transaction = null;
+  HibernateUtil hibernateUtil;
+  
+  public StudentDAO(HibernateUtil hibernateUtil) {
+    this.hibernateUtil = hibernateUtil;
+  }
+
+  public boolean addStudent(Student student) {
+    try (Session session = hibernateUtil.getSessionFactory().openSession()) {
+      // start a transaction
+      transaction = session.beginTransaction();
+      // save the student object
+      session.save(student);
+     
+      // commit transaction
+      transaction.commit();
+      return true;
+    } catch (Exception e) {
+      if (transaction != null) {
+        transaction.rollback();
+      }
+      e.printStackTrace();
+      return false;
     }
-	
+  }
+
+  public List<Student> getStudents() {
+    try (Session session = hibernateUtil.getSessionFactory().openSession()) {
+      return session.createQuery("from Student", Student.class).list();
+    }
+  }
 }
